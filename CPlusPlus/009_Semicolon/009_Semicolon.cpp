2172cfc4938494ab81e69d416caf04f9d62d50ec
Ratisser <w_vista@naver.com>
2023-03-13 15:49:39 +0900

    2023/03/13 수업내용 009  ~ 013

---
+++ CPlusPlus/009_Semicolon/009_Semicolon.cpp
@@ -0,0 +1,11 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    // 변수명이나 메모리명의 첫번째 글자로 숫자를 사용할 수 없다.
+    // ex) int 1val = 10;
+
+    int val = 10;
+    // ';'(Semicolon)이란 코드를 마무리짓는 문자이다.
+}
---
+++ CPlusPlus/011_Operator003/011_Operator003.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    // 삼항 연산자
+    // a ? b : c
+
+    // 조건연산 ? 참일때 메모리 : 거짓일 때 메모리
+
+    int Left = 7;
+    int Right = 3;
+    int Result = 0;
+
+    Result = (Left != Right) ? 100 : 200; // 100
+    Result = true ? 100 : 200; // 100
+    Result = false ? 100 : 200; // 200
+    // RAM에 올라간다면 삼항 전부 올라가야된다.
+    // 100도 있어야 연산이 가능하다.
+    // 메모리 어딘가에 저장되어 있다.
+
+    // sizeof연산자 단항 연산자
+    // () 안에 있는 메모리영역의 바이트 크기를 리턴합니다.
+    // sizeof(메모리영역)
+    // sizeof(int 영역)
+    Result = sizeof(int); // 4
+    Result = sizeof(bool); // 1
+    Result = sizeof(Left); // 4
+}
