d1d5c474507f9e53e9016c30a4b2f966ab74481d
Ratisser <w_vista@naver.com>
2023-03-14 15:51:40 +0900

    2023/03/14 수정 내용 : 019_Inline

--- CPlusPlus/019_Inline/019_Inline.cpp
+++ CPlusPlus/019_Inline/019_Inline.cpp
@@ -1,7 +1,16 @@
 ﻿
 #include <iostream>
 
+
+inline void Test()
+{
+    printf_s("Function");
+}
 int main()
 {
-    printf_s(); // inline은 무엇인가?
+    Test();
+    // 'inlien'은 Test();자리에 Test()함수의 내용을 복붙한다.
+    // 함수를 사용할 수록 프로그램이 느려지기 때문에 사용하는 기능이다.
+    // 함수를 복붙하는 느낌으로 스택을 만들지 않도록
+    // 최적화할 수 있다면 해달라는 코드이다.
 }
