e7e9479ea4d062657bcdf7a9686b84e217253392
조대규 <zxvk1030@naver.com>
2023-03-23 20:29:59 +0900

    test

--- CPlusPlus/HomeWork0323/HomeWork0323.cpp
+++ CPlusPlus/HomeWork0323/HomeWork0323.cpp
@@ -1,7 +1,55 @@
 ﻿
 #include <iostream>
 
+// 자신이 논리적으로 이 연산자를 이렇게 만들면 되겠다 할 수 있는 함수 다 만들기
+
+class Number
+{
+private:
+    int Value = 2;
+public:
+    Number(int Val) : Value(Val)
+    {
+
+    }
+
+    int operator + (int _Value)
+    {
+        return _Value + Value;
+    }
+
+    int operator - (int _Value)
+    {
+        return _Value - Value;
+    }
+
+    int operator * (int _Value)
+    {
+        return _Value * Value;
+    }
+
+    int operator / (int _Value)
+    {
+        return _Value / Value;
+    }
+
+    int operator % (int _Value)
+    {
+        return _Value % Value;
+    }
+
+    int operator [] (size_t Val)
+    {
+
+    }
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+    {
+        Number Value = 10;
+
+        Value + 10;
+        Value.operator+(10); // operator 연산자 겹지정 함수라 부른다.
+        Value[30];
+    }
 }
