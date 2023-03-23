68338c5829300cdb54f10760455d0f3c7684c665
조대규 <zxvk1030@gmail.com>
2023-03-23 13:45:45 +0900

    2023/03/23 수정 내용 : EX2, 046

--- CPlusPlus/046_BytePadding/046_BytePadding.cpp
+++ CPlusPlus/046_BytePadding/046_BytePadding.cpp
@@ -3,13 +3,18 @@
 
 class Player
 {
-private:
+public:
     
-    int Hp;
-    short a2;
-    int Att;
-    short a;
+    int Hp = 10;       // 4
+    short MP = 20;     // 4
+    int Att = 40;      // 4
+    short Def = 50;    // 4
     
+    //int Hp = 10;       // 8
+    //short MP = 20;     // 0
+    //int Att = 40;      // 8
+    //__int64 Def = 50;  // 8
+
     // std::vector와 같듯이 제일 큰 변수 기준으로 패딩된다.
 
 };
@@ -19,5 +24,22 @@ int main()
     Player NewPlayer;
     int Size = sizeof(NewPlayer);
 
-    printf_s("%d", Size);
+    // 자료형 size
+    int intsize = sizeof(int);
+    __int64 int64size = sizeof(__int64);
+
+    // 주소 형변환
+    __int64 AddressHp = (__int64) & NewPlayer.Hp;
+    int* PtrHp = (int*)AddressHp;
+    short* PtrMP = (short*)(AddressHp + intsize); // + 4
+    int* PtrAtt = (int*)(AddressHp + intsize * 2); // + 8
+    short* PtrDef = (short*)(AddressHp + intsize * 3); // + 12
+
+    // 결과적으로 패딩된 공간에 처음 주소에 저장이 된다.
+    printf_s("  Hp : %d\n", *PtrHp);
+    printf_s("  Mp : %d\n", *PtrMP);
+    printf_s(" Att : %d\n", *PtrAtt);
+    printf_s(" Def : %d\n\n", *PtrDef);
+
+    printf_s("Size : %d\n", Size);
 }
---
+++ CPlusPlus/EX2/EX2.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int a[2] = { 0b0000000000000000000000000000000001, 0b0000000000000000000000000000000001 };
+
+    int* Ptr = &a[0];
+    __int64 Address = (__int64)Ptr;
+    
+    int* Ptra0 = (int*)Address;
+
+    int* Ptra1 = (int*)(Address + 1);
+
+    int aa = 00000000000000000000000100000000;
+
+    printf_s("주소0 : %d\n", *Ptra0);
+    printf_s("주소1 : %d\n", *Ptra1);
+    printf_s("주소2 : %d\n", aa);
+}
