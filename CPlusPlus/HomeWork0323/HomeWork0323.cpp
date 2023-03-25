b661a43b87a92e3dcc3b95f980b35d8c6760a9d9
조대규 <zxvk1030@naver.com>
2023-03-26 01:36:40 +0900

    2023/03/26 숙제(미완)

--- CPlusPlus/HomeWork0323/HomeWork0323.cpp
+++ CPlusPlus/HomeWork0323/HomeWork0323.cpp
@@ -8,57 +8,22 @@ class Number
 private:
     int Value;
 public:
-    Number(int Val) : Value(Val) {}
+    Number(int Val) : Value(Val) 
+    {
 
-    // 산술연산자
-    int operator + (int _Value) // +
-    {
-        return Value + _Value;
     }
-    int operator - (int _Value) // -
+#pragma region 단항 연산자 & 증감 연산자
+
+    int operator + ()
     {
-        return Value - _Value;
-    }
-    int operator * (int _Value) // *
-    {
-        return Value * _Value;
-    }
-    int operator / (int _Value) // /
-    {
-        return Value / _Value;
-    }
-    int operator % (int _Value) // %
-    {
-        return Value % _Value;
+        return Value;
     }
 
-    // 비트단위 연산자
-    int operator ~ () // ~
+    int operator - ()
     {
-        return ~Value;
-    }
-    int operator & (int _Value) // AND
-    {
-        return Value & _Value;
-    }
-    int operator | (int _Value) // OR
-    {
-        return Value | _Value;
-    }
-    int operator ^ (int _Value) // XOR
-    {
-        return Value ^ _Value;
-    }
-    int operator >> (int _Value) // >> Shift
-    {
-        return Value >> _Value;
-    }
-    int operator << (int _Value) // >> Shift
-    {
-        return Value << _Value;
+        return -Value;
     }
 
-    // 증감 연산자 ('전위'만 구현했고 '후위'는 구현하지 못했음)
     Number& operator ++ () // ++Value
     {
         ++Value;
@@ -69,57 +34,224 @@ public:
         --Value;
         return *this;
     }
-
-    Number& operator ++(int) // ++을 뒤에 써야하니까 인자가 필요하지만 이것은 인자가 필요한 연산자가 아니기 때문에 쓸 수 없는 인자를 넣어준다.
+    // ++을 뒤에 써야하니까 인자가 필요하지만 이것은 인자가 필요한 연산자가 아니기 때문에 쓸 수 없는 인자를 넣어준다.
+    int operator ++ (int) // Value++
     {
-        Number Obj = Value;
+        int Temp = Value;
         ++Value; // Value는 ++해주고 이전값을 리턴한다.
-        return Obj;
+        return Temp;
     }
-    Number& operator --(int)
+    int operator -- (int) // Value--
     {
-        Number Obj = Value;
+        int Temp = Value;
         --Value;
-        return Obj;
+        return Temp;
     }
 
-    // 비교연산자
-    bool operator == (int _Value) // ==
+#pragma endregion
+
+#pragma region 산술 연산자
+
+    int operator + (const int _Value) // +
+    {
+        return Value + _Value;
+    }
+    int operator - (const int _Value) // -
+    {
+        return Value - _Value;
+    }
+    int operator * (const int _Value) // *
+    {
+        return Value * _Value;
+    }
+    int operator / (const int _Value) // /
+    {
+        return Value / _Value;
+    }
+    int operator % (const int _Value) // %
+    {
+        return Value % _Value;
+    }
+
+#pragma endregion
+
+#pragma region 비트 연산자
+
+    int operator ~ () // ~
+    {
+        return ~Value;
+    }
+    int operator & (const int _Value) // AND
+    {
+        return Value & _Value;
+    }
+    int operator | (const int _Value) // OR
+    {
+        return Value | _Value;
+    }
+    int operator ^ (const int _Value) // XOR
+    {
+        return Value ^ _Value;
+    }
+    int operator >> (const int _Value) // >> Shift
+    {
+        return Value >> _Value;
+    }
+    int operator << (const int _Value) // >> Shift
+    {
+        return Value << _Value;
+    }
+
+#pragma endregion
+
+#pragma region 논리 연산자
+
+    bool operator && (const int _Value) // AND
+    {
+        return  Value && _Value;
+    }
+    bool operator || (const int _Value) // OR
+    {
+        return  Value || _Value;
+    }
+    bool operator ! () // NOT
+    {
+        return  !Value;
+    }
+
+#pragma endregion
+
+#pragma region 비교 연산자
+
+    bool operator == (const int _Value) // ==
     {
         return  Value == _Value;
     }
-    bool operator != (int _Value) // !=
+    bool operator != (const int _Value) // !=
     {
         return Value != _Value;
     }
-    bool operator < (int _Value) // <
+    bool operator < (const int _Value) // <
     {
         return Value < _Value;
     }
-    bool operator > (int _Value) // >
+    bool operator > (const int _Value) // >
     {
         return Value > _Value;
     }
-    bool operator <= (int _Value) // <=
+    bool operator <= (const int _Value) // <=
     {
         return Value <= _Value;
     }
-    bool operator >= (int _Value) // >=
+    bool operator >= (const int _Value) // >=
     {
         return Value >= _Value;
     }
+#pragma endregion
+
+#pragma region 대입 연산자
+
+    int operator = (const int _Value)
+    {
+        return _Value;
+    }
+    Number& operator +=(const int _Value) // 기존값에 + 결과 대입
+    {
+        Value += _Value;
+        return *this;
+    }
+    Number& operator -=(const int _Value) // 기존값에 - 결과 대입
+    {
+        Value -= _Value;
+        return *this;
+    }
+    Number& operator *=(const int _Value) // 기존값에 * 결과 대입
+    {
+        Value *= _Value;
+        return *this;
+    }
+    Number& operator /=(const int _Value) // 기존값에 / 결과 대입
+    {
+        Value /= _Value;
+        return *this;
+    }
+    Number& operator %=(const int _Value) // 기존값에 % 결과 대입
+    {
+        Value %= _Value;
+        return *this;
+    }
+    Number& operator <<= (const int _Value) // 기존값에 Right Shift 결과 대입
+    {
+        Value <<= _Value;
+        return *this;
+    }
+    Number& operator >>=(const int _Value) // 기존값에 Left Shift 결과 대입 
+    {
+        Value >>= _Value;
+        return *this;
+    }
+    Number& operator &=(const int _Value) // 기존값에 AND 결과 대입
+    {
+        Value &= _Value;
+        return *this;
+    }
+    Number& operator |=(const int _Value) // 기존값에 OR 결과 대입
+    {
+        Value |= _Value;
+        return *this;
+    }
+    Number& operator ^=(const int _Value) // 기존값에 XOR 결과 대입
+    {
+        Value ^= _Value;
+        return *this;
+    }
+
+#pragma endregion
+
+#pragma region 멤버 & 포인터 연산자 . & * [] ->
+    
+    // 주소가 반환이 되어야 하고 입력값은 객체이다. 
+    // 메모리 최적화를 위해 call by reference
+    int* operator & ()
+    {
+        return &(this->Value);
+    }
+    int operator * ()
+    {
+        return Value;
+    }
+    int operator -> () // 진짜 모르겠음
+    {
+        return Value;
+    }
+    int operator [] (int _Value)
+    {
+        return 
+    }
+
+#pragma endregion 
+    
+#pragma region 삼항 연산자
+    //int operator a?b: (int a, int b)
+    //{
+    //     안되는듯
+    //}
+#pragma endregion 
+
 };
 int main()
 {
     Number Object = 2;
 
-    Object + 10; // 12
+    int num = Object + 10; // 12
     Object - 10; // -8
     Object * 10; // 20
     Object / 10; // 0
     Object % 10; // 2
 
-    
+
+    int ObjPtr = *Object;
+    ObjPtr = (int) & Object;
     ~Object; // 0010 -> 1101 : -3
 
              //  [-3인지 확인 과정]   1101
--- CPlusPlus/HomeWork0324/HomeWork0324.cpp
+++ CPlusPlus/HomeWork0324/HomeWork0324.cpp
@@ -1,8 +1,292 @@
 ﻿
-
 #include <iostream>
 #include <conio.h>
-#include <Windows.h>;
+#include <Windows.h>
+
+
+void BombTimeCounting(short* _BombTimerArr, const short _MaxBombNum)
+{
+	for (size_t i = 0; i < _MaxBombNum; i++)
+	{
+		if (_BombTimerArr[i] > 1)
+		{
+			--_BombTimerArr[i];
+		}
+	}
+}
+
 int main()
 {
+	
+	// 기본 맵 생성
+	const short ScreenYSize = 20;
+	const short ScreenXSize = 20;
+	short MapDataArr[ScreenYSize][ScreenXSize];
+
+	// 맵 초기화
+	for (short y = 0; y < ScreenYSize; y++)
+	{
+		for (short x = 0; x < ScreenXSize; x++)
+		{
+			if (x == 0 || y == 0 || x == ScreenXSize - 1 || y == ScreenYSize - 1)
+			{
+				MapDataArr[y][x] = 1;
+			}
+			else
+			{
+				MapDataArr[y][x] = 0;
+			}
+		}
+	}
+
+	const short ScreenFrame = 10;
+	short Window = 1;
+
+	const short MaxBombNum = 5; // 폭탄 설치 개수
+	const short TimeOfBomb = 50; // 폭탄의 시간
+	short BombTimerArr[MaxBombNum] = { 0, }; // 폭탄마다 시간 저장
+
+	short BombLocationArr[ScreenYSize][ScreenXSize] = {0,};
+	short BombNumCount = 0; // 설치된 폭탄 개수
+
+	// 플레이어 좌표
+	int PlayerY = ScreenYSize / 2;
+	int PlayerX = ScreenXSize / 2;
+
+	// 맵 출력 시 구조물 구분 숫자
+	const short EmptySpaceIconNum = 0;
+	const short WallIconNum = 1;
+	const short InstalledWallIconNum = 2;
+	const short BombIconNum = 5;
+	const short BombAnimCount = 2;
+
+	while (true)
+	{
+		// 화면 초기화
+		system("cls");
+
+		for (short y = 0; y < ScreenYSize; y++)
+		{
+			for (short x = 0; x < ScreenXSize; x++)
+			{
+				if (MapDataArr[y][x] >= BombIconNum)
+				{
+					if (BombTimerArr[BombLocationArr[y][x]] == 1)
+					{
+						if (MapDataArr[y][x] == BombIconNum)
+						{
+							MapDataArr[y - 1][x] = BombIconNum + (BombAnimCount * 2) + 1;
+							MapDataArr[y + 1][x] = BombIconNum + (BombAnimCount * 2) + 1;
+							MapDataArr[y][x - 1] = BombIconNum + (BombAnimCount * 2) + 1;
+							MapDataArr[y][x + 1] = BombIconNum + (BombAnimCount * 2) + 1;
+						}
+						else if (MapDataArr[y][x] == BombIconNum + BombAnimCount)
+						{
+							MapDataArr[y - 2][x] = BombIconNum + (BombAnimCount * 2) + 1;
+							MapDataArr[y + 2][x] = BombIconNum + (BombAnimCount * 2) + 1;
+							MapDataArr[y][x - 2] = BombIconNum + (BombAnimCount * 2) + 1;
+							MapDataArr[y][x + 2] = BombIconNum + (BombAnimCount * 2) + 1;
+						}
+						else if (MapDataArr[y][x] == BombIconNum + (BombAnimCount * 2))
+						{
+							BombTimerArr[BombLocationArr[y][x]] = 0;
+
+							MapDataArr[y][x] = 0;
+							MapDataArr[y - 2][x] = 0;
+							MapDataArr[y - 1][x] = 0;
+							MapDataArr[y + 1][x] = 0;
+							MapDataArr[y + 2][x] = 0;
+
+							MapDataArr[y][x - 2] = 0;
+							MapDataArr[y][x - 1] = 0;
+							MapDataArr[y][x + 1] = 0;
+							MapDataArr[y][x + 2] = 0;
+						}
+						if (MapDataArr[y][x] != 0)
+						{
+							MapDataArr[y][x]++;
+						}
+					}
+				}
+			}
+		}
+
+		// 화면 출력
+		for (short y = 0; y < ScreenYSize; y++)
+		{
+			for (short x = 0; x < ScreenXSize; x++)
+			{
+				if (x == PlayerX && y == PlayerY)
+				{
+					printf_s("●");
+				}
+				else {
+					switch (MapDataArr[y][x])
+					{
+					case EmptySpaceIconNum:
+						printf_s("  ");
+						break;
+
+					case WallIconNum:
+						printf_s("■");
+						break;
+
+					case InstalledWallIconNum:
+						printf_s("▣");
+						break;
+
+					case BombIconNum:
+						printf_s("δ");
+						break;
+
+					default:
+						printf_s("★");
+						break;
+					}
+				}
+			}
+			printf_s("\n");
+		}
+
+		// 실시간 폭탄 개수 카운트
+		BombNumCount = 0;
+		for (short i = 0; i < MaxBombNum; i++)
+		{
+			if (BombTimerArr[i] > 0)
+			{
+				BombNumCount++;
+			}
+		}
+
+		// 폭탄 시간 출력 화면
+		if (Window == 1)
+		{
+			// 폭탄시간 배열 출력
+			for (short i = 0; i < MaxBombNum; i++)
+			{
+				printf_s("%2d ", BombTimerArr[i]);
+			}
+			printf_s("\n설치된 폭탄 수 : %d", BombNumCount);
+		}
+		
+		// 키를 누르지 않을 때 작업
+		if (0 == _kbhit())
+		{
+			Sleep(ScreenFrame);
+
+			// 키 입력 없을 시 폭탄 시간 감소
+			BombTimeCounting(BombTimerArr, MaxBombNum);
+
+			continue;
+		}
+
+		char Ch = _getch();
+
+		// 키 입력 중 폭탄 시간 감소
+		BombTimeCounting(BombTimerArr, MaxBombNum);
+
+		// 키 입력
+		switch (Ch)
+		{
+		case 'w':
+		case 'W':
+			if (MapDataArr[PlayerY - 1][PlayerX] == 1 || MapDataArr[PlayerY - 1][PlayerX] == 2)
+			{
+				break;
+			}
+			else
+			{
+				--PlayerY;
+			}
+			break;
+
+		case 's':
+		case 'S':
+			if (MapDataArr[PlayerY + 1][PlayerX] == 1 || MapDataArr[PlayerY + 1][PlayerX] == 2)
+			{
+				break;
+			}
+			else
+			{
+				++PlayerY;
+			}
+			break;
+
+		case 'a':
+		case 'A':
+			if (MapDataArr[PlayerY][PlayerX - 1] == 1 || MapDataArr[PlayerY][PlayerX - 1] == 2)
+			{
+				break;
+			}
+			else
+			{
+				--PlayerX;
+			}
+			break;
+
+		case 'd':
+		case 'D':
+			if (MapDataArr[PlayerY][PlayerX + 1] == 1 || MapDataArr[PlayerY][PlayerX + 1] == 2)
+			{
+				break;
+			}
+			else
+			{
+				++PlayerX;
+			}
+			break;
+
+		// 장애물 설치
+		case 'g':  
+		case 'G':
+			if (MapDataArr[PlayerY][PlayerX] != 0)
+			{
+				break;
+			}
+			MapDataArr[PlayerY][PlayerX] = 2;
+			break;
+
+		// 폭탄
+		case 'f':
+		case 'F':
+
+			// 폭탄은 빈 공간일 경우에만 설치 가능하다.
+			// 폭탄의 최대 설치 수 제한
+			if(MapDataArr[PlayerY][PlayerX] != 0 || BombNumCount == MaxBombNum)
+			{
+				break;
+			}
+			
+			MapDataArr[PlayerY][PlayerX] = BombIconNum;
+
+
+			// 위치 저장
+			
+			for (short i = 0; i < MaxBombNum; i++)
+			{
+				if (BombTimerArr[i] == 0)
+				{
+					BombLocationArr[PlayerY][PlayerX] = i;
+					BombTimerArr[i] = TimeOfBomb;
+					break;
+				}
+			}
+
+			break;
+		case 'm':
+		case 'M':
+			if (Window == 0)
+			{
+				Window++;
+			}
+			else {
+				Window--;
+			}
+			
+		default:
+			break;
+		}
+
+		Sleep(ScreenFrame);
+	}
 }
