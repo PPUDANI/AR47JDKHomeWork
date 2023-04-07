18ffe959551845bbfa391c10ce2b93f33e753830
조대규 <zxvk1030@naver.com>
2023-04-07 15:50:46 +0900

    2023/04/07 수업 내용

--- CPlusPlus/GameEngineBase/GameEngineDebug.h
+++ CPlusPlus/GameEngineBase/GameEngineDebug.h
@@ -6,13 +6,16 @@
 //class GameEngineDebug
 //{
 //public:
+// // Constrcuter destructer
 //	GameEngineDebug();
 //	~GameEngineDebug();
 //
+//  // Delete Function
 //	GameEngineDebug(const GameEngineDebug& _Other) = delete;
 //	GameEngineDebug(GameEngineDebug&& _Other) noexcept = delete;
 //	GameEngineDebug& operator=(const GameEngineDebug& _Other) = delete;
 //	GameEngineDebug& operator=(GameEngineDebug&& _Other) noexcept = delete;
+// 
 //protected:
 //
 //private:
