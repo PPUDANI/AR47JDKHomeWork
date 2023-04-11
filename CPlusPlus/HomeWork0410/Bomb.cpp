8dc2d844223f2286a2cef255f92ace41d5cbd3be
조대규 <zxvk1030@naver.com>
2023-04-11 11:22:52 +0900

    2023/04/11 숙제 제출

--- CPlusPlus/HomeWork0410/Bomb.cpp
+++ CPlusPlus/HomeWork0410/Bomb.cpp
@@ -21,6 +21,7 @@ void Bomb::Update()
 
 	if (0 == --BoomCount)
 	{
+		RenderChar = '#';
 		BoomState = BoomState::Frame1;
 	}
 
@@ -30,31 +31,34 @@ void Bomb::Update()
 void Bomb::Render()
 {
 	ConsoleGameObject::Render();
+
+	// Update 후에 Render가 되서 초기화되므로 여기서 함.. 
 	switch (BoomState)
 	{
 	case BoomState::Idle:
 		break;
 
-	case BoomState::Frame2:
+	case BoomState::Frame2: // Frame2때도 Frame1이 실행되어야 해서 break를 걸지 않았음.
 		for (int i = -2; i <= 2; i += 4)
 		{
 			int2 SidePosX = Pos;
 			int2 SidePosY = Pos;
 			SidePosX.X += i;
 			SidePosY.Y += i;
-			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(SidePosX, '@');
-			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(SidePosY, '@');
+			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(SidePosX, '#');
+			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(SidePosY, '#');
 		}
-	case BoomState::Frame1:
 
+	case BoomState::Frame1:
+		
 		for (int i = -1; i <= 1; i += 2)
 		{
 			int2 SidePosX = Pos;
 			int2 SidePosY = Pos;
 			SidePosX.X += i;
 			SidePosY.Y += i;
-			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(SidePosX, '@');
-			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(SidePosY, '@');
+			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(SidePosX, '#');
+			ConsoleGameScreen::GetMainScreen().SetScreenCharacter(SidePosY, '#');
 		}
 
 		if (BoomState::Frame2 == BoomState)
