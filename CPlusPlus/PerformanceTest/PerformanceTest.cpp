8e775e436a2d37477dda30f2ed6ff74776df45ca
조대규 <zxvk1030@naver.com>
2023-05-04 02:07:02 +0900

    2023/05/04 성능 측정코드(시간 세기)

---
+++ CPlusPlus/PerformanceTest/PerformanceTest.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+#include <windows.h>
+class A 
+{
+    int a = 0;
+};
+class B : public A
+{
+    int b = 0;
+};
+int main()
+{
+    LARGE_INTEGER Count;
+    LARGE_INTEGER Start;
+    LARGE_INTEGER End;
+
+    QueryPerformanceFrequency(&Count);
+    QueryPerformanceCounter(&Start);
+    
+
+    for (int i = 0; i < 5000; i++)
+    {
+        std::cout << i;
+        system("cls");
+        
+        {
+            B* PtrB = new B();
+            A* Test = dynamic_cast<A*>(PtrB);
+        }
+    }
+
+    QueryPerformanceCounter(&End);
+
+    double Tick = static_cast<double>(End.QuadPart - Start.QuadPart) / static_cast<double>(Count.QuadPart);
+
+    float floatTick = static_cast<float>(Tick);
+    printf("%.4f초 걸렸습니다.", floatTick);
+
+}
