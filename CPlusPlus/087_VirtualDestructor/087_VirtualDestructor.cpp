1a457dfb902f7f6ee3e68990a625dc8626c9b4f1
조대규 <zxvk1030@naver.com>
2023-05-10 23:00:13 +0900

    2023/05/10 가상 소멸자, map 실험

---
+++ CPlusPlus/087_VirtualDestructor/087_VirtualDestructor.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+class A
+{
+public:
+    A() 
+    {
+
+    }
+
+    // 소멸자의 특수성
+    // ~컴파일러는 소멸자의 이름을 ~로 생각한다. (~A 도 ~, A를 상속받은 ~B도 ~ 임)
+    // virtual을 붙이지 않으면 일반적인 함수와 동일하게 판단한다.
+    // this 인자로 인한 문제이다.
+    // 부모의 virtual을 붙인다.
+    virtual ~A()
+    {
+
+    }
+};
+class B : public A
+{
+public:
+    B()
+    {
+
+    }
+    ~B()
+    {
+
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ CPlusPlus/EX9/EX9.cpp
@@ -0,0 +1,52 @@
+﻿
+#include <iostream>
+#include <map>
+#include <GameEngineBase/GameEngineDebug.h>
+class A
+{
+    int a;
+public:
+    A() : a(3)
+    {
+    }
+    
+};
+
+int main()
+{
+    GameEngineDebug::LeckCheck();
+    std::map<int, A*> MapA;
+    
+    for (int i = 0; i < 10; i++)
+    {
+        A* newA = new A();
+        MapA.insert(std::make_pair(i, newA));
+    }
+
+    for (const std::pair<int, A*> Pair : MapA)
+    {
+        A* ObjA = Pair.second;
+        if (nullptr != ObjA)
+        {
+            delete ObjA;
+            ObjA = nullptr;
+        }
+    }
+
+    for (int i = 10; i < 20; i++)
+    {
+        A* newA = new A();
+        MapA.insert(std::make_pair(i, newA));
+    }
+
+    for (const std::pair<int, A*> Pair : MapA)
+    {
+        A* ObjA = Pair.second;
+        if (nullptr != ObjA)
+        {
+            delete ObjA;
+            ObjA = nullptr;
+        }
+    }
+
+}
